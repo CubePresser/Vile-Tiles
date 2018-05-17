32b90cd5c6bb217016edc98cf77affa37beada4b
jonesjonathan <jonesjon@oregonstate.edu>
2018-05-16 17:20:56 -0700

    Basic game created.
    Basic gameplay elements are mostly present. Tile selection (both player and random computer selection) is implemented. Player looking and environment also added. Performance is fine right now but some code is sloppy and needs to be refactored. For instance, the random tile selection is not optimal and would benefit from an integer array of tile statuses so it doesn't have to dig deep into the tiles' encapuslated items. UI elements, endgame, scoring, timer and menu still need to be implemented.

---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour {
+
+	public GameObject[] tiles;
+	public float frequency; //Seconds between tile switching
+	private float start_frequency;
+	public int tiles_remaining;
+	private int num_tiles;
+	private TileScript tile;
+	// Use this for initialization
+	void Start () {
+		tiles = GameObject.FindGameObjectsWithTag("Tile"); //Gets a list of tiles
+		tiles_remaining = num_tiles = tiles.Length;
+		start_frequency = frequency;
+		tile = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		frequency -= Time.deltaTime;
+		if(frequency < 0 && tiles_remaining > 0)
+		{
+			if(tile != null && tile.status == -5)
+			{
+				tile.UpdateMaterial(0);
+			}
+			frequency = start_frequency;
+			int depth = 100;
+			int rand = Random.Range(0, num_tiles);
+			while((tile = tiles[rand].GetComponent<TileScript>()).status != 0 && tiles_remaining > 0) //Performance heavy, need to change
+			{
+				if(depth == 0)
+				{
+					tiles_remaining = 0;
+					break;
+				}
+				rand = Random.Range(0, num_tiles); //Gets a tile with default status
+				depth--;
+			}
+			tile.UpdateMaterial(-5);
+		}
+	}
+}
---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour {
+
+	public Material default_material, hit_material, missed_material, selected_material;
+	public GameScript game_script;
+	public int status; // 0 default, 1 hit, -1 missed, -5 = selected
+	private Renderer renderer;
+
+	public void UpdateMaterial (int value) {
+		switch(value) {
+			case 0:
+				renderer.material = default_material;
+				break;
+			case 1:
+				renderer.material = hit_material;
+				game_script.tiles_remaining--;
+				break;
+			case -1:
+				renderer.material = missed_material;
+				if(status != 1)
+					game_script.tiles_remaining--;
+				break;
+			case -5:
+				renderer.material = selected_material;
+				break;
+		}
+		status = value;
+	}
+
+	// Use this for initialization
+	void Start () {
+		renderer = GetComponent<Renderer>();
+		status = 0; //Default status 
+	}
+	private void OnTriggerStay() {
+		if(Input.GetKeyDown("space"))
+		{
+			if(status == -5)
+			{
+				UpdateMaterial(1);
+			} 
+			else
+			{
+				UpdateMaterial(-1);
+			}
+		}
+	}
+}
